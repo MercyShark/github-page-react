b385eaf3859b496796bfae71cc57c096099c8184
Rishabh <prajapatirishabh04@gmail.com>
2025-03-25 15:57:59 +0530

    added more lines

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React + Rishabh</h1>
+      <h1>Vite + React + add more lines</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
